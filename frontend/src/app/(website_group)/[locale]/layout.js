1d5e5ef78b46b8beee43522caea3b6bb43df7295
devkkd <developmentkontentkraftdigital.com>
2026-06-08 14:32:55 +0530

    fix: header footer on all pages, locale only wraps homepage

--- frontend/src/app/(website_group)/[locale]/layout.js
+++ frontend/src/app/(website_group)/[locale]/layout.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { LOCALES, DEFAULT_LOCALE } from "@/middleware";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import QuestionsBanner from "@/components/QuestionsBanner";
-import FloatingActions from "@/components/FloatingActions";
 import { LocaleProvider } from "@/context/LocaleContext";
 
-// Tell Next.js which locale slugs to pre-render at build time
 export function generateStaticParams() {
   return LOCALES.map((locale) => ({ locale }));
 }
@@ -17,15 +12,7 @@ export default async function LocaleLayout({ children, params }) {
 
   return (
     <LocaleProvider locale={validLocale}>
-      <div className="flex flex-col min-h-screen w-full relative">
-        <Header locale={validLocale} />
-        <div className="grow">
-          {children}
-        </div>
-        <QuestionsBanner />
-        <Footer locale={validLocale} />
-        <FloatingActions />
-      </div>
+      {children}
     </LocaleProvider>
   );
 }
